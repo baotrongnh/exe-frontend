9d1e276a0599f44c8896f1444f60b1fafb9d125d
Bao Trong <nhbaotrong@gmail.com>
2025-10-01 16:03:15 +0700

    fix: route signup

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -48,7 +48,7 @@ export default function Navbar() {
                   Đăng nhập
                 </Link>
                 <Link
-                  href="/sign-up"
+                  href="/signup"
                   className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-lg text-sm font-medium transition-colors"
                 >
                   Đăng ký
